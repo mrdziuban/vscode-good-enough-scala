2c45abfe58e579ce0e669721b23940dd98dda935
Matt Dziuban <mrdziuban@gmail.com>
2018-06-07 18:23:45 -0400

    Add hover provider to show all matching terms.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,73 +1,98 @@
 import {
   createConnection,
+  Hover,
   Location,
   ProposedFeatures,
   TextDocuments,
   TextDocumentPositionParams,
-  WorkspaceFolder
+  WorkspaceFolder,
+  MarkupKind
 } from "vscode-languageserver";
 import * as fs from "fs";
 import * as path from "path";
 import * as url from "url"
 
-// Create a connection for the server. The connection uses Node's IPC as a transport.
-// Also include all preview / proposed LSP features.
 const connection = createConnection(ProposedFeatures.all);
+const documents = new TextDocuments();
+documents.listen(connection);
 
-// Create a simple text document manager. The text document manager
-// supports full document sync only
-const documents: TextDocuments = new TextDocuments();
+connection.onInitialize(() => ({
+  capabilities: {
+    definitionProvider: true,
+    hoverProvider: true,
+    textDocumentSync: documents.syncKind
+  }
+}));
 
-connection.onInitialize(() => ({ capabilities: { definitionProvider: true } }));
+function symName(name: string): string { return `__SCALA_SYMBOL__${name}`; }
 
 function flatten<A>(arr: A[][]): A[] { return [].concat.apply([], arr); }
+
+function now(): number { return +(new Date()); }
+
+function sortBy<A, B>(fn: (a: A) => B, arr: A[]): A[] {
+  const sorters = {
+    number: (x: number, y: number): number => x - y,
+    any: (x: any, y: any): number => x < y ? -1 : (x > y) ? 1 : 0
+  };
+  return arr.sort((a1: A, a2: A) => {
+    const [b1, b2] = [fn(a1), fn(a2)];
+    return (typeof b1 === "number" && typeof b2 === "number")
+      ? sorters.number(b1, b2)
+      : sorters.any(b1, b2);
+  });
+}
+
+interface ScalaFile {
+  absolutePath: string;
+  relativePath: string;
+};
 
 function getScalaFiles(dir: string, files: string[] = []): string[] {
   return files.concat(flatten(fs.readdirSync(dir).map((file: string) => {
     const joined = path.resolve(path.join(dir, file));
-    if (fs.statSync(joined).isDirectory()) {
-      return getScalaFiles(joined, files);
-    } else if (/\.scala$/.test(joined)) {
-      return [joined];
-    } else {
-      return [];
-    }
+    return fs.statSync(joined).isDirectory()
+      ? getScalaFiles(joined, files)
+      : (/\.scala$/.test(joined) ? [joined] : []);
   })));
 }
 
-type StrObj<V> = { [k: string]: V };
-
 interface ScalaSymbol {
   symbolName: string;
-  file: string;
+  file: ScalaFile;
   location: { line: number; character: number; };
 }
 
-const indexedFiles: StrObj<(() => void)[]> = {};
-const symbols: StrObj<ScalaSymbol[]> = {};
+// type IndexedFiles = { [file: string]: { [sym: string]: number[] } };
+// let indexedFiles: IndexedFiles = {};
 
-function updateIndexedFiles(): void {
-  Object.keys(symbols).forEach((sym: string) => {
-    symbols[sym].forEach((symbol: ScalaSymbol, idx: number) => {
-      indexedFiles[symbol.file] = indexedFiles[symbol.file] || [];
-      // TODO - figure out splice
-      indexedFiles[symbol.file].push(() => symbols[sym] = symbols[sym].splice(1, idx));
-      indexedFiles[symbol.file].push(() => symbols[sym].filter((s: ScalaSymbol) => s !== symbol));
-    });
-  });
-}
+type Symbols = { [sym: string]: ScalaSymbol[] };
+let symbols: Symbols = {};
 
+// function updateIndexedFiles(): void {
+//   indexedFiles = Object.keys(symbols).reduce(
+//     (acc: IndexedFiles, s: string) => {
+//       symbols[s].forEach((sym: ScalaSymbol, idx: number) => {
+//         const abs = sym.file.absolutePath;
+//         acc[abs] = acc[abs] || {};
+//         acc[abs][sym.symbolName] = acc[abs][sym.symbolName] || [];
+//         acc[abs][sym.symbolName].push(idx);
+//       });
+//       return acc;
+//     },
+//     {});
+// }
 
-type SymbolExtractor = (f: string, c: string) => ScalaSymbol[];
+type SymbolExtractor = (f: ScalaFile, c: string) => ScalaSymbol[];
 
-function extractMatches(rx: RegExp, symbolType: string, file: string): (line: string, lineNum: number) => ScalaSymbol[] {
+function extractMatches(rx: RegExp, symbolType: string, file: ScalaFile): (line: string, lineNum: number) => ScalaSymbol[] {
   return (line: string, lineNum: number) => {
-    const offset = symbolType.length + 1;
+    const offset = symbolType.length + 2;
     const retVal = [];
     let matches = rx.exec(line);
     while (!!matches) {
       retVal.push({
-        symbolName: matches[1],
+        symbolName: symName(matches[1]),
         file,
         location: { line: lineNum, character: matches.index + offset }
       });
@@ -80,8 +105,8 @@ function extractMatches(rx: RegExp, symbolType: string, file: string): (line: st
 const alphaRx = /[a-zA-Z]/;
 const termRx = new RegExp(`[${alphaRx.source.slice(1, -1)}0-9_]`);
 
-function defaultExtractor(symbolType: string): (f: string, c: string) => ScalaSymbol[] {
-  return (file: string, contents: string) => {
+function defaultExtractor(symbolType: string): (f: ScalaFile, c: string) => ScalaSymbol[] {
+  return (file: ScalaFile, contents: string) => {
     const rx = new RegExp(`${symbolType} (${alphaRx.source}${termRx.source}+)`, "g");
     return flatten(contents.split("\n").map(extractMatches(rx, symbolType, file)));
   };
@@ -95,12 +120,13 @@ const symbolExtractors: SymbolExtractor[] = [
   defaultExtractor("def")
 ];
 
-function getScalaSymbols(file: string): ScalaSymbol[] {
-  const contents = fs.readFileSync(file).toString();
+function getScalaSymbols(file: ScalaFile): ScalaSymbol[] {
+  const contents = fs.readFileSync(file.absolutePath).toString();
   return flatten(symbolExtractors.map((ex: SymbolExtractor) => ex(file, contents)));
 }
 
-connection.onInitialized(() => {
+function update(): void {
+  const start = now();
   connection.workspace.getWorkspaceFolders()
     .then((fldrs: WorkspaceFolder[] | null) => {
       const folders = flatten((fldrs || [])
@@ -109,27 +135,26 @@ connection.onInitialized(() => {
           const path = url.parse(f.uri).path;
           return !!path ? [[f, path]] : [];
         }));
-      return flatten(folders.map((t: [WorkspaceFolder, string]) => getScalaFiles(t[1])));
+      return flatten(folders.map(([_, dir]: [WorkspaceFolder, string]): ScalaFile[] => {
+        const files = getScalaFiles(dir);
+        return files.map((f: string) => ({ absolutePath: f, relativePath: f.replace(`${dir}/`, "") }));
+      }));
     })
-    .then((files: string[]) =>
-      Promise.all(files.map((file: string) =>
+    .then((files: ScalaFile[]) =>
+      Promise.all(files.map((file: ScalaFile) =>
         new Promise((resolve: (syms: ScalaSymbol[]) => void) => resolve(getScalaSymbols(file))))))
     // This is the wrong type but the compiler is complaining. See cast below.
     .then((symsPromise: Promise<ScalaSymbol[][]>) => {
-      const syms: ScalaSymbol[] = flatten(<any>symsPromise as ScalaSymbol[][]);
-      syms.forEach((sym: ScalaSymbol) => {
-        symbols[sym.symbolName] = symbols[sym.symbolName] || [];
-        symbols[sym.symbolName].push(sym);
-      });
-      updateIndexedFiles();
-      connection.console.log("finished indexing");
-      connection.console.log(JSON.stringify(symbols, null, 2));
+      symbols = flatten(<any>symsPromise as ScalaSymbol[][]).reduce((acc: Symbols, sym: ScalaSymbol) => {
+        acc[sym.symbolName] = (acc[sym.symbolName] || []).concat([sym]);
+        return acc;
+      }, {});
+      // updateIndexedFiles();
+      connection.console.log(`Finished indexing ${Object.keys(symbols).length} scala symbols in ${now() - start}ms`);
     });
-});
+}
 
-// TODO
-// connection.onDidChangeWorkspaceFolders()
-// connection.onHover()
+connection.onInitialized(update);
 
 function buildTerm(line: string, char: number): string {
   const append = (term: string, changeIdx: (i: number) => number, concat: (newChar: string, term: string) => string): string => {
@@ -147,70 +172,34 @@ function buildTerm(line: string, char: number): string {
   return term;
 }
 
-connection.onDefinition((tdp: TextDocumentPositionParams): Location[] => {
+function getTerm(tdp: TextDocumentPositionParams): string | undefined {
   const file = url.parse(tdp.textDocument.uri).path;
-  if (!file) { return []; }
+  if (!file) { return; }
   const line = fs.readFileSync(file).toString().split("\n")[tdp.position.line];
-  const term = buildTerm(line, tdp.position.character);
-  return (symbols[term] || []).map((symbol: ScalaSymbol) =>
-    Location.create(`file://${symbol.file}`, { start: symbol.location, end: symbol.location }));
+  return symName(buildTerm(line, tdp.position.character));
+}
+
+connection.onDefinition((tdp: TextDocumentPositionParams): Location[] => {
+  const term = getTerm(tdp);
+  return ((term && symbols[term]) || []).map((sym: ScalaSymbol) =>
+    Location.create(`file://${sym.file.absolutePath}`, { start: sym.location, end: sym.location }));
 });
 
-// // This handler provides the initial list of the completion items.
-// connection.onCompletion((_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-//   // The pass parameter contains the position of the text document in
-//   // which code complete got requested. For the example we ignore this
-//   // info and always provide the same completion items.
-//   return [
-//   {
-//     label: 'TypeScript',
-//     kind: CompletionItemKind.Text,
-//     data: 1
-//   },
-//   {
-//     label: 'JavaScript',
-//     kind: CompletionItemKind.Text,
-//     data: 2
-//   }
-//   ]
-// });
-
-// // This handler resolve additional information for the item selected in
-// // the completion list.
-// connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
-//   if (item.data === 1) {
-//     item.detail = 'TypeScript details',
-//     item.documentation = 'TypeScript documentation'
-//   } else if (item.data === 2) {
-//     item.detail = 'JavaScript details',
-//     item.documentation = 'JavaScript documentation'
-//   }
-//   return item;
-// });
-
-/*
-connection.onDidOpenTextDocument((params) => {
-  // A text document got opened in VSCode.
-  // params.uri uniquely identifies the document. For documents store on disk this is a file URI.
-  // params.text the initial full content of the document.
-  connection.console.log(`${params.textDocument.uri} opened.`);
+connection.onHover((tdp: TextDocumentPositionParams): Hover | undefined => {
+  const term = getTerm(tdp);
+  return (term && symbols[term])
+    ? {
+      contents: {
+        kind: MarkupKind.Markdown,
+        value: sortBy((sym: ScalaSymbol) => sym.file.absolutePath, symbols[term]).map((sym: ScalaSymbol) => {
+          const line = `${sym.location.line + 1},${sym.location.character}`;
+          return `- [${sym.file.relativePath}:${line}](file://${sym.file.absolutePath}#L${line})`;
+        }).join("\n")
+      }
+    }
+    : undefined;
 });
-connection.onDidChangeTextDocument((params) => {
-  // The content of a text document did change in VSCode.
-  // params.uri uniquely identifies the document.
-  // params.contentChanges describe the content changes to the document.
-  connection.console.log(`${params.textDocument.uri} changed: ${JSON.stringify(params.contentChanges)}`);
-});
-connection.onDidCloseTextDocument((params) => {
-  // A text document got closed in VSCode.
-  // params.uri uniquely identifies the document.
-  connection.console.log(`${params.textDocument.uri} closed.`);
-});
-*/
 
-// Make the text document manager listen on the connection
-// for open, change and close text document events
-documents.listen(connection);
+connection.onDidSaveTextDocument(update);
 
-// Listen on the connection
 connection.listen();
