1e301989a3bb15d50fa8362769b8c59c10796c40
Matt Dziuban <mrdziuban@gmail.com>
2018-06-07 18:23:46 -0400

    Clean up client.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,5 +1,5 @@
 import * as path from "path";
-import { workspace, ExtensionContext } from "vscode";
+import { ExtensionContext } from "vscode";
 
 import {
   LanguageClient,
@@ -11,33 +11,16 @@ import {
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-
-  // The server is implemented in node
-  let serverModule = context.asAbsolutePath(path.join("server", "out", "server.js"));
-  // The debug options for the server
-  let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
-
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
-  let serverOptions: ServerOptions = {
+  const serverModule = context.asAbsolutePath(path.join("server", "out", "server.js"));
+  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+  const serverOptions: ServerOptions = {
     run : { module: serverModule, transport: TransportKind.ipc },
     debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
-  }
+  };
+  const clientOptions: LanguageClientOptions = { documentSelector: [{ scheme: "file", language: "scala" }] };
 
-  // Options to control the language client
-  let clientOptions: LanguageClientOptions = {
-    // Register the server for plain text documents
-    documentSelector: [{ scheme: "file", language: "scala" }],
-    synchronize: {
-      // Notify the server about file changes to ".clientrc files contain in the workspace
-      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-    }
-  }
-
-  // Create the language client and start the client.
   client = new LanguageClient("languageServerExample", "Language Server Example", serverOptions, clientOptions);
-
-  // Start the client. This will also launch the server
+  // Starting the client also launches the server
   client.start();
 }
 
