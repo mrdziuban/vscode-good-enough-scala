46439ca0488ebeb4f19c3e5296795bb0fbba1bdd
Matt Dziuban <mrdziuban@gmail.com>
2019-02-19 17:51:42 -0500

    Do custom URI encoding.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
-import { ExtensionContext } from "vscode";
+import { ExtensionContext, Uri } from "vscode";
 import { LanguageClient, TransportKind } from "vscode-languageclient";
+import * as url from "url";
 
 let client: LanguageClient;
 
@@ -11,7 +12,15 @@ export function activate(context: ExtensionContext): void {
     "goodEnoughScalaLSP",
     '"Good Enough" Scala LSP',
     { run: baseOpts, debug: Object.assign(baseOpts, { options: { execArgv: ["--nolazy", "--inspect=6009"] } }) },
-    { documentSelector: [{ scheme: "file", language: "scala" }] });
+    {
+      documentSelector: ["scala"],
+      uriConverters: {
+        // VS Code by default %-encodes even the colon after the drive letter
+        // NodeJS handles it much better
+        code2Protocol: (uri: Uri) => url.format(url.parse(uri.toString(true))),
+        protocol2Code: (str: string) => Uri.parse(str)
+      }
+    });
   // Starting the client also launches the server
   client.start();
 }
