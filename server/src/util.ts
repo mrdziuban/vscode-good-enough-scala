96bef7856bb51129979992141787c69e286be47b
Matt Dziuban <mrdziuban@gmail.com>
2019-02-19 17:51:43 -0500

    Fix isPromise implementation.

--- server/src/util.ts
+++ server/src/util.ts
@@ -1,4 +1,3 @@
-export const isPromise = <A>(a: {}): a is PromiseLike<A> =>
-  Object.prototype.hasOwnProperty.call(a, "then") && typeof (<any>a).then === "function";
+export const isPromise = <A>(a: {}): a is PromiseLike<A> => typeof (<any>a).then === "function";
 export const now = () => +(new Date());
 export const withOpt = <A, O>(fn: (a: A) => O) => (a?: A): O | undefined => a ? fn(a) : undefined;
