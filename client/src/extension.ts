33b929f386436775314ffe8842bc83ac7bfbbd9d
Matt Dziuban <mrdziuban@gmail.com>
2019-02-19 17:51:43 -0500

    Prepare for publishing.

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -10,7 +10,7 @@ export function activate(context: ExtensionContext): void {
   const baseOpts = { module: serverModule, transport: TransportKind.ipc };
   client = new LanguageClient(
     "goodEnoughScalaLSP",
-    '"Good Enough" Scala LSP',
+    '"Good Enough" Scala Language Server',
     { run: baseOpts, debug: Object.assign(baseOpts, { options: { execArgv: ["--nolazy", "--inspect=6009"] } }) },
     {
       documentSelector: ["scala"],
